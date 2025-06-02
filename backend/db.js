c83cbbe49ffd13d5ca514ef35003090bff82fbd7
jakula01 <u_uzz@inbox.ru>
2025-06-02 23:39:09 +0500

    last

--- backend/db.js
+++ backend/db.js
@@ -7,9 +7,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 module.exports = pool;
